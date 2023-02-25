db538622ece5d573c556cc8da0f3071d7b36c86d
Avery <averygadd7@gmail.com>
2023-02-24 17:00:24 -0700

    fixed bugs and added refresh/clear function

--- script.js
+++ script.js
@@ -7,9 +7,9 @@
 //create different color options from RGB to B/W
 //Have fun :) and get it done on time with little distractions
 
-const normalColor = '#333333';
+const normalColor = 'rgb(36, 34, 34)';
 const normalSize = 32;
-const normalMode = 'color';
+const normalMode = 'normal';
 
 let currentColor = normalColor;
 let currentSize = normalSize;
@@ -23,13 +23,16 @@ const gridValue = document.getElementById('gridValue');
 const sizeBar = document.getElementById('sizeBar');
 const grid = document.getElementById('grid');
 
-rainbowBtn.onclick = () => document.setMode('rainbow');
-normalBtn.onclick = () => document.setMode('normal');
-eraserBtn.onclick = () => document.setMode('eraser');
-clearBtn.onclick 
+rainbowBtn.onclick = () => setMode('rainbow');
+normalBtn.onclick = () => setMode('normal');
+eraserBtn.onclick = () => setMode('eraser');
+clearBtn.onclick = () => gridRefresh();
 sizeBar.onmousemove = (e) => updateValue(e.target.value);
 sizeBar.onchange = (e) => updateSize(e.target.value);
 
+let mouseDown = false
+document.body.onmousedown = () => (mouseDown = true);
+document.body.onmouseup = () => (mouseDown = false);
 
 function setColor(newColor) {
   currentColor = newColor;
@@ -38,50 +41,65 @@ function setSize(newSize) {
   currentSize = newSize;
 };
 function setMode(newMode) {
-  currentMode = newMode;
+  btnOn(newMode)
+  currentMode = newMode
 };
+
 //grid functions
 function setGrid(size) {
   grid.style.gridTemplateColumns = `repeat(${size}, 1fr)`
   grid.style.gridTemplateRows = `repeat(${size}, 1fr)`
 
-  for (i = 0; i < size * size; i++) {
-    let gridSquare = document.createElement('div');
-    gridSquare.className = 'grid-square';
+  for (let i = 0; i < size * size; i++) {
+    const gridSquare = document.createElement('div');
+    gridSquare.classList.add('grid-square')
     gridSquare.addEventListener('mouseover', changeColor)
     gridSquare.addEventListener('mousedown', changeColor)
-    grid.appendChild(gridSquare);
-  };
+    grid.appendChild(gridSquare)
+  }
 };
 function updateValue(value) {
-  gridValue.textContent = `${value} x ${value}`
+  gridValue.innerHTML = `${value} x ${value}`
 }
 function updateSize(value) {
-  setSize()
-  updateValue()
+  setSize(value)
+  updateValue(value)
+  gridRefresh()
 }
+window.onload = () => {
+  setGrid(normalSize);
+  btnOn(normalMode);
+};
+function clear() {
+  grid.textContent = '';
+}
+function gridRefresh() {
+  clear()
+  setGrid(currentSize)
+}
+
 //buttons that change the color on click
 function changeColor(e) {
-  if (e.type === 'mouseover' && 'mousedown') return;
+  if (e.type === 'mouseover' && !mouseDown) return
   if (currentMode === 'rainbow') {
     randomRed = Math.floor(Math.random() * 256)
     randomGreen = Math.floor(Math.random() * 256)
     randomBlue = Math.floor(Math.random() * 256)
-    e.target.style.backgroundColor = `rgb(${randomRed}, ${randomGreen}, ${randomBlue}`;
+    e.target.style.backgroundColor = `rgb(${randomRed}, ${randomGreen}, ${randomBlue})`;
   }else if (currentMode === 'normal'){
     e.target.style.backgroundColor = currentColor;
   }else if (currentMode === 'eraser'){
-    e.target.style.backgroundColor = rgb(255, 255, 255);
+    e.target.style.backgroundColor = '#FFFFFF';
   }
 }
 
-let btns = document.getElementsByClassName('btn');
-
-for (let i = 0; i < btns.length; i++) {
-  btns[i].addEventListener('click', function() {
+function btnOn(newMode) {
+  let btns = document.getElementsByClassName('btn');
+  for (let i = 0; i < btns.length; i++) {
+    btns[i].addEventListener('click', function() {
     let currentMode = document.getElementsByClassName('active');
     currentMode[0].className = currentMode[0].className.replace(' active', "");
     this.className += ' active';
   });
+  }
 }
-
